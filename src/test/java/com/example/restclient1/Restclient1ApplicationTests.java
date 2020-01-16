d0ad82439f03524cffdeec82dec2076347bd7d25
Sisi <dqgsh1055@gmail.com>
2020-01-16 20:12:54 +0900

    Initial commit

---
+++ src/test/java/com/example/restclient1/Restclient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restclient1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
